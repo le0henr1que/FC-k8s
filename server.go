417b494cd26ca29a26c7060c78f7737af3e7be9b
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2024-01-15 16:50:01 -0300

    first commit

---
+++ server.go
@@ -0,0 +1,11 @@
+package main 
+
+import "net/http"
+
+func main(){
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("Hello full cycle !!!"))
+	})
+
+	http.ListenAndServe(":80", nil)
+}
