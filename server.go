ff698bc24a1b4a6f131e22553dfa6087f2a90b15
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2024-02-11 15:55:33 -0300

    feat: hpa

--- server.go
+++ server.go
@@ -40,15 +40,14 @@ func ConfigMap(w http.ResponseWriter, r *http.Request) {
 }
 
 func Healthz(w http.ResponseWriter, r *http.Request) {
-
-	duration := time.Since(startedAt)
-
-	if duration.Seconds() > 25 {
-		w.WriteHeader(500)
-		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
-	} else {
-		w.WriteHeader(200)
-		w.Write([]byte("ok"))
-	}
-
+  duration := time.Since(startedAt)
+ 
+  if duration.Seconds() < 10 {
+    w.WriteHeader(500)
+    w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+  } else {
+    w.WriteHeader(200)
+    w.Write([]byte("ok"))
+  }
 }
+ 
